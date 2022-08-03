15c10b74148f2bc3bf6039369ea372b5a472ae66
Will Myers <56778753+willmyethewebsiteguy@users.noreply.github.com>
2022-08-03 19:15:02 -0400

    Update SplitNav.js

--- SplitNav.js
+++ SplitNav.js
@@ -40,5 +40,34 @@
     if (!stylesheet) return;
     document.head.prepend(stylesheet);
   }
+
+
+  /*
+  * Check If Flex Gap Is Supported
+  * Delete by Jan 2023 if no issues have popped up with this
+  **/
+  function checkFlexGap() {
+    // create flex container with row-gap set
+    var flex = document.createElement("div");
+    flex.style.display = "flex";
+    flex.style.flexDirection = "column";
+    flex.style.rowGap = "1px";
+
+    // create two, elements inside it
+    flex.appendChild(document.createElement("div"));
+    flex.appendChild(document.createElement("div"));
+
+    // append to the DOM (needed to obtain scrollHeight)
+    document.body.appendChild(flex);
+    var isSupported = flex.scrollHeight === 1; // flex container should be 1px high from the row-gap
+    flex.parentNode.removeChild(flex);
+
+    return isSupported;
+  }
+  if(checkFlexGap){
+    //console.log('flex gap supported!')
+  } else {
+    //console.log('flex gap not supported :(')
+  }
   
-}())
+}());
